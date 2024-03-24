097d18c6441e33c977097128e9119f4e4a64d347
Nav1306 <ce20b018@iittp.ac.in>
2024-03-24 19:33:57 +0530

    fourth

--- router/importHotelDataRouter.js
+++ router/importHotelDataRouter.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = express.Router();
-const hotels = require("../data/hotels");
-const { Hotel } = require("../model/hotels");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const router = express.Router();
+// const hotels = require("../data/hotels");
+// const { Hotel } = require("../model/hotels");
 
-router.route("/add")
-.post (async (req,res) => {
-    try {
-        await Hotel.deleteMany();
-        const hotelsInDb =  await Hotel.insertMany(hotels.data);
-        res.json(hotelsInDb);
-    } catch (error) {
-        res.json({message : "Could not add data to the database"})
-        console.log(error);
-    }
+// router.route("/add")
+// .post (async (req,res) => {
+//     try {
+//         await Hotel.deleteMany();
+//         const hotelsInDb =  await Hotel.insertMany(hotels.data);
+//         res.json(hotelsInDb);
+//     } catch (error) {
+//         res.json({message : "Could not add data to the database"})
+//         console.log(error);
+//     }
    
-})
+// })
 
-module.exports = router;
+// module.exports = router;
